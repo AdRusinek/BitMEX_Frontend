15cdb613936e5c6f234c05c24da354d75c766096
AdRusinek <rusinekadrian19@gmail.com>
2020-03-08 00:56:44 +0100

    fontawesome

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -28,7 +28,7 @@ class Header extends Component {
                             className="nav-link"
                             to="/trailing"
                         >
-                            Set Trailing Stop
+                            Trailing Stop
                         </Link>
                     </li>
                 </ul>
